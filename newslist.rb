3e4a039f8d956b47977fdec75a987ccca14f1f8c
kpugal@qwinixtech.com <kavithapugal@Kavithas-MacBook-Pro.local>
2016-05-22 21:15:24 +0530

    changed to zadd
    zadd doesn’t take duplicate data

--- newslist.rb
+++ newslist.rb
@@ -4,7 +4,7 @@ class NewsList
 
 	REDIS_KEY = 'NEWS_XML'
 	NUM_NEWS = 20
-	TRIM_THRESHOLD = 500
+	TRIM_THRESHOLD = 1000
 
 	def initialize
 		@db = Redis.new
@@ -14,16 +14,17 @@ class NewsList
 
 	def push(data)
 		#list - NEWS_XML 
-		#p "inside push "
-		@db.rpush(REDIS_KEY, data)
-		#p "length of list stored at key :::"+(@db.llen(REDIS_KEY)).to_s
+		p "inside push "
+		# zadd or sadd - dosent store duplicate data
+		@db.zadd(REDIS_KEY, @trim_count, data)
+		#p "length of sorted list ::::"+(@db.zcard(REDIS_KEY)).to_s
 		@trim_count += 1
-		if(@trim_count > TRIM_THRESHOLD)
-			#p @trim_count.to_s+" trim_count ****************  Before---"
-			@db.ltrim(REDIS_KEY, 0, NUM_NEWS)
-			@trim_count =0
-			#p @trim_count.to_s+" trim_count ****************   After ------"
-		end
+		#if(@trim_count > TRIM_THRESHOLD)
+			#@db.zremrangebyrank(REDIS_KEY, 0, NUM_NEWS)
+			#@trim_count = 0
+			#p "length of sorted list ::::"+(@db.zcard(REDIS_KEY)).to_s
+			#p (@db.zrange(REDIS_KEY, 0, 10, WITHSCORES)).to_s
+		#end
 	end
 
 
